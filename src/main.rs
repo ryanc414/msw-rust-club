a093c98647474e1b9300d2fc942fa3f3cea87e6c
Ryan Collingham <ryanc118@gmail.com>
2018-03-20 00:33:01 +0000

    minor tidy up

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ fn main() {
     // Roll and score the dice until told not to.
     let mut between = Range::new(1u16, 7);
     let mut rng = rand::thread_rng();
-    let num_rolls = 6;
+    let num_dice = 6;
 
-    roll(num_rolls, &mut rng, &mut between);
+    roll(num_dice, &mut rng, &mut between);
 
     while cont() {
-        roll(num_rolls, &mut rng, &mut between);
+        roll(num_dice, &mut rng, &mut between);
     }
 }
 
@@ -33,10 +33,9 @@ fn cont() -> bool {
     }
 }
 
-fn roll<R: Rng>(num_rolls: u16, mut rng: &mut R, mut between: &mut Range<u16>) {
+fn roll<R: Rng>(num_dice: u16, mut rng: &mut R, mut between: &mut Range<u16>) {
     // Roll and score a set of dice.
-    let dice = roll_dice(num_rolls, &mut rng, &mut between);
-
+    let dice = roll_dice(num_dice, &mut rng, &mut between);
     print_dice(&dice);
     println!("Score = {}", score_dice(&dice));
 }
@@ -44,11 +43,9 @@ fn roll<R: Rng>(num_rolls: u16, mut rng: &mut R, mut between: &mut Range<u16>) {
 fn print_dice(dice: &Vec<u16>) {
     // Pretty print a set of dice.
     println!("Dice:");
-
     for d in dice.iter() {
         print!("{} ", d);
     }
-
     println!("");
 }
 
@@ -66,13 +63,8 @@ fn score_die(die: &u16) -> u16 {
     }
 }
 
-fn roll_dice<R: Rng>(num_rolls: u16, mut rng: &mut R, mut between: &mut Range<u16>) -> Vec<u16> {
+fn roll_dice<R: Rng>(num_rolls: u16, mut rng: &mut R, between: &mut Range<u16>) -> Vec<u16> {
     // Roll a set of dice using provided Rng and range.
-    c![roll_die(&mut rng, &mut between), for _i in 0..num_rolls]
-}
-
-fn roll_die<R: Rng>(mut rng: &mut R, between: &mut Range<u16>) -> u16 {
-    // Roll a single die using provided Rng and range.
-    between.sample(&mut rng)
+    c![between.sample(&mut rng), for _i in 0..num_rolls]
 }
 
